b1b4d3d0e0a54e67bb6207c804e07f34009b14e5
ikudza <kudashin-ivan@ya.ru>
2025-06-20 13:49:42 +0300

    Update logic mirroring

--- mirroring_kafka/logic.py
+++ mirroring_kafka/logic.py
@@ -1,9 +1,9 @@
 import asyncio
-from datetime import datetime
+
 
 from mirroring_kafka.logger import log
 from mirroring_kafka.utils import (
-    KafkaSettings, get_consumer, get_producer, Past
+    KafkaSettings, get_consumer, get_producer
 )
 
 PAST_LAG = 30*60
@@ -15,8 +15,6 @@ async def run_mirroring(
     stopping: asyncio.Event,
 ):
     log.info('Mirroring started')
-    past_data = await get_last(dest_kafka_settings)
-    past_ids = {x for _, x in past_data}
     while not stopping.is_set():
 
         async with get_consumer(
@@ -30,16 +28,6 @@ async def run_mirroring(
                 for pt, messages in data.items():
                     for msg in messages:
                         count['read'] += 1
-                        if past_data and past_data[-1][0] >= msg.timestamp:
-                            if msg.key in past_ids:
-                                offsets[pt] = msg.offset + 1
-                                count['skipped'] += 1
-                                log.debug({
-                                    "message": 'Skip message',
-                                    "offset": msg.offset,
-                                    "src_topic": src_kafka_settings.topic,
-                                })
-                                continue
 
                         await dest_producer.send_and_wait(
                             topic=dest_kafka_settings.topic,
@@ -52,57 +40,16 @@ async def run_mirroring(
 
             if offsets:
                 # say only if we have offset something
-                log.info(
-                    {
-                        "message": 'Mirroring done',
-                        "read": count["read"],
-                        "sent": count["sent"],
-                        "skipped": count["skipped"],
-                        "src_topic": src_kafka_settings.topic,
-                        "dest_topic": dest_kafka_settings.topic,
-                    }
-                )
+                log.debug({
+                    "message": 'Mirroring done',
+                    "read": count["read"],
+                    "sent": count["sent"],
+                    "skipped": count["skipped"],
+                    "src_topic": src_kafka_settings.topic,
+                    "dest_topic": dest_kafka_settings.topic,
+                })
                 await src_consumer.commit(offsets)
 
         await asyncio.sleep(5)
 
     log.info('Mirroring stopped')
-
-
-async def get_last(dest_kafka_settings: KafkaSettings) -> Past:
-    async with get_consumer(
-        dest_kafka_settings,
-        [dest_kafka_settings.topic],
-    ) as dest_consumer:
-        offsets = await dest_consumer.end_offsets(dest_consumer.assignment())
-        past: Past = []
-        earlier = datetime.utcnow().timestamp()
-        for tp, offset in offsets.items():
-            if offset > 0:
-                dest_consumer.seek(tp, offset - 1)
-                msg = await dest_consumer.getone()
-                earlier = min(earlier, msg.timestamp)
-                past.append((msg.timestamp, msg.key))
-
-        _ts = earlier - PAST_LAG
-        new_offsets = await dest_consumer.offsets_for_times(
-            {tp: _ts for tp in offsets}
-        )
-        for tp, ot in new_offsets.items():
-            if ot is not None:
-                dest_consumer.seek(tp, ot.offset)
-
-        data = await dest_consumer.getmany(timeout_ms=10000, max_records=1000)
-
-        for pt, messages in data.items():
-            for msg in messages:
-                past.append((msg.timestamp, msg.key))
-
-        log.info({
-            "message": 'Got last messages',
-            "dest_topic": dest_kafka_settings.topic,
-            "count": len(past),
-            "earlier": f"{datetime.fromtimestamp(earlier)}",
-        })
-        past.sort()
-        return past
--- mirroring_kafka/utils.py
+++ mirroring_kafka/utils.py
@@ -50,7 +50,7 @@ async def get_consumer(
     consumer = aiokafka.AIOKafkaConsumer(
         *topics,
         bootstrap_servers=settings.servers,
-        group_id='mirroring_kafka_vq1',
+        group_id='mirroring_kafka_vq2',
         fetch_max_wait_ms=1000,
         sasl_mechanism=settings.sasl_mechanism,
         security_protocol=settings.security_protocol,
@@ -58,7 +58,7 @@ async def get_consumer(
         sasl_plain_username=settings.username,
         sasl_plain_password=settings.password,
         enable_auto_commit=False,
-        auto_offset_reset='earliest',
+        auto_offset_reset='latest',
         max_poll_records=100,
     )
     log.debug(f'Init consumer: {topics}')
